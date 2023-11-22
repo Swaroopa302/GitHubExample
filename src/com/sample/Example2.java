6ee3565d0faa51ba2929a87b15070a3b40379f95
Swaroopa302 <swaroopathanda302@gmail.com>
2023-11-22 11:43:49 +0530

    2nd commit of github

---
+++ src/com/sample/Example2.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("2ND Example of github");
+	}
+
+}
